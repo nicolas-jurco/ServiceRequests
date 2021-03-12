0889cf3067ed63597db5f799642415eed0c7cc70
aknicolas <anicolas.kis@gmail.com>
2021-03-12 16:19:38 -0300

    Moved all configurations to app.settings

--- Services/EmailServices.cs
+++ Services/EmailServices.cs
@@ -1,4 +1,5 @@
-﻿using ServiceRequests.Models;
+﻿using Microsoft.Extensions.Configuration;
+using ServiceRequests.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +10,11 @@ namespace ServiceRequests.Services
 {
     public class EmailServices : IEmailServices
     {
-#if DEBUG
-        private MailAddress From = new MailAddress("nico_k30@hotmail.com");
-#else
-        private MailAddress From =  new MailAddress("<founded email goes here>");
-#endif
+        IConfiguration _configuration;
+        public EmailServices(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         private readonly object email_locker = new object();
         /// <summary>
         /// Function to send emails
@@ -25,18 +26,34 @@ namespace ServiceRequests.Services
             lock (email_locker) {
                 var _message = new MailMessage();
 
-                _message.From = this.From;
+                _message.From = new MailAddress(_configuration.GetValue<string>("SmtpSettings:Email"));
 
-                _message.Subject = $"Service GUID {serviceModel.Id} has changed status to {serviceModel.CurrentStatus}"
+                _message.Subject = $"Service GUID {serviceModel.Id} has changed status to {serviceModel.CurrentStatus}";
                 // Assuming we have a list of addresses, emails and names
                 _message.To.Add(LookForCreatorEmail(serviceModel.CreatedBy));
                 _message.Body = $"<h1> <<< message withh all service model information goes here>>> </h1>";
 
                 _message.IsBodyHtml = true;
 
-                using (var smtp = new SmtpClient())
+                try
                 {
-                    smtp.Send(_message);
+                    using (var smtp = new SmtpClient())
+                    {
+                        smtp.UseDefaultCredentials = false;
+                        smtp.Host = _configuration.GetValue<string>("SmtpSettings:Host");
+                        //"smtp-mail.outlook.com";
+                        smtp.Port = _configuration.GetValue<int>("SmtpSettings:Port");
+                        smtp.Credentials = new System.Net.NetworkCredential(
+                             _configuration.GetValue<string>("SmtpSettings:Email"),
+                             _configuration.GetValue<string>("SmtpSettings:Password"));
+                        smtp.EnableSsl = true;
+                        smtp.Send(_message);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    throw;
                 }
             }
         }
