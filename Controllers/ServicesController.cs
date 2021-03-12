d9c168300b6c2a4c88a3664f5f936f4d7325432f
aknicolas <anicolas.kis@gmail.com>
2021-03-12 15:28:26 -0300

    Modified ServicesController Added all required Methods (async to make it scalable) Added all responses

--- Controllers/ServicesController.cs
+++ Controllers/ServicesController.cs
@@ -1,47 +1,114 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ServiceRequests.Models;
+using ServiceRequests.Service;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
-namespace ServiceRequests.Models
+namespace ServiceRequests.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
     public class ServicesController : ControllerBase
     {
-        // GET: api/<ValuesController>
+        private readonly DatabaseContext _context;
+
+        //Injected logger to use swagger and context to use db
+        public ServicesController(DatabaseContext context, ILogger<ServicesController> logger)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        private readonly ILogger<ServicesController> _logger;
+
+        // GET: ServiceModels
         [HttpGet]
-        public IEnumerable<string> Get()
+        [Route("api/[controller]/[action]")]
+        public ActionResult<ServiceModel> Index()
         {
-            return new string[] { "value1", "value2" };
+            var serviceModels = _context.Services.ToArrayAsync();
+            if (serviceModels == null)
+                return StatusCode(204, "Empty content");
+            return Ok(serviceModels);
         }
 
-        // GET api/<ValuesController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        // GET: ServiceModels/Details/5
+        [HttpGet]
+        [Route("api/[controller]/[action]")]
+        public async Task<ActionResult> Details(Guid? id)
         {
-            return "value";
+            var serviceModel = await _context.Services.Where(m => m.Id == id).ToArrayAsync();
+            if (serviceModel == null)
+                return NotFound();
+            return Ok(serviceModel);
         }
 
-        // POST api/<ValuesController>
+
+        // POST: ServiceModels/Create
         [HttpPost]
-        public void Post([FromBody] string value)
+        [Route("api/[controller]/[action]")]
+        public async Task<ActionResult> Create([Bind("Id,BuildingCode,Description,CurrentStatus,CreatedBy,CreatedDate,LastModifiedBy,LastModifiedDate")] ServiceModel _serviceModel)
         {
+            if (ModelState.IsValid)
+            {
+                //Following line creates a new GUID on DEBUG mode
+#if DEBUG
+                _serviceModel.Id = Guid.NewGuid();
+#endif
+                _context.Add(_serviceModel);
+                await _context.SaveChangesAsync();
+                return Ok(_serviceModel);
+            }
+            return BadRequest(_serviceModel);
+
         }
 
-        // PUT api/<ValuesController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [HttpPut]
+        [Route("api/[controller]/[action]")]
+        public async Task<ActionResult> Update([Bind("Id,BuildingCode,Description,CurrentStatus,CreatedBy,CreatedDate,LastModifiedBy,LastModifiedDate")] ServiceModel _serviceModel)
         {
+            var serviceModel = _context.Services.Where(m => m.Id == _serviceModel.Id).ToArray().FirstOrDefault();
+
+            if (serviceModel == null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(serviceModel.Copy(_serviceModel));
+                    await _context.SaveChangesAsync();
+                }
+                catch (Exception)
+                {
+                    return BadRequest();
+                }
+            }
+            return Ok();
         }
 
-        // DELETE api/<ValuesController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        // DELETE: ServiceModels/Delete/5
+        [HttpDelete]
+        [Route("api/[controller]/[action]")]
+        public async Task<ActionResult> Delete(Guid? id)
         {
+            var serviceModel = _context.Services.Where(m => m.Id == id).FirstOrDefault();
+            if (serviceModel == null)
+            {
+                return NotFound(id);
+            }
+            else
+            {
+                _context.Services.Remove(serviceModel);
+                await _context.SaveChangesAsync();
+            }
+            return StatusCode(201, "Successfully deleted");
         }
     }
 }
